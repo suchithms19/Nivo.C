517ec38a18df753162590b6c556b15df08aa3ecb
suchithms19 <mssuchith2@gmail.com>
2025-12-25 14:55:51 +0530

    Add appointment booking API and service registrations

---
+++ Nivo.API/Controllers/AppointmentsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nivo.API.Models.DTO;
+using Nivo.API.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace Nivo.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AppointmentsController : ControllerBase
+    {
+        private readonly AppointmentService _appointmentService;
+
+        public AppointmentsController(AppointmentService appointmentService)
+        {
+            _appointmentService = appointmentService;
+        }
+
+        [HttpPost("{userId}/book")]
+        public async Task<IActionResult> BookAppointment(string userId, [FromBody] BookAppointmentDto dto)
+        {
+            try
+            {
+                var (appointment, patient) = await _appointmentService.BookAppointmentAsync(userId, dto);
+
+                return Ok(new
+                {
+                    message = "Appointment booked successfully",
+                    appointment,
+                    patient
+                });
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(new { error = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { error = "Something went wrong" });
+            }
+        }
+        }
+    }
+}
--- Nivo.API/Program.cs
+++ Nivo.API/Program.cs
@@ -1,14 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Nivo.API.Data;
+using Nivo.API.Repositories.Implementation;
+using Nivo.API.Repositories.Interface;
+using Nivo.API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+builder.Services.AddScoped<IPatientRepository, PatientRepository>();
+builder.Services.AddScoped<AppointmentService>();
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
