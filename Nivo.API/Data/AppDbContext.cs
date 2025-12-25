fa110ffa482dc01aa446bcdd4181c9baa6d136b9
suchithms19 <mssuchith2@gmail.com>
2025-12-25 14:44:19 +0530

    Add EF Core DbContext and remove default weather API

---
+++ Nivo.API/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Nivo.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Nivo.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        //This avoids nullable warnings.
+
+        public DbSet<Patient> Patients { get; set; } = null!;
+        public DbSet<Appointment> Appointments { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Appointment>()
+                .HasOne(a => a.Patient)
+                .WithMany(p => p.Appointments)
+                .HasForeignKey(a => a.PatientId);
+        }
+    }
+}
